c87468531cc88f36a8ab4165b461791b80829a37
LLe_Breton <laurelebreton8@gmail.com>
2018-03-15 16:38:05 +0100

    class DTM improved + test DTMascReader

--- src/main/java/eu/ensg/jade/semantic/DTM.java
+++ src/main/java/eu/ensg/jade/semantic/DTM.java
@@ -1,6 +1,6 @@
 package eu.ensg.jade.semantic;
 
-import java.util.List;
+import java.util.Map;
 
 import eu.ensg.jade.geometricObject.ISurfacicObject;
 
@@ -19,6 +19,14 @@ public class DTM implements ISurfacicObject {
 	 */
 	private double[][] tabDTM;
 	
+	/**
+	 * The header containing the metadata of the DTM (same order as in the ascii file)*
+	 * 
+	 * Header Keys : ncols - nrows - xllcorner - yllcorner - cellsize
+	 */
+	private Map<String,Double> headerDTM;
+	
+	
 // ========================== CONSTRUCTORS =========================	
 
 	/**
@@ -26,9 +34,31 @@ public class DTM implements ISurfacicObject {
 	 * 
 	 * @param tabDTM the table associates to the DTM
 	 */
-	public DTM(double[][] tabDTM) {
+	public DTM(double[][] tabDTM,Map<String,Double> headerDTM) {
 		this.tabDTM = tabDTM;
+		this.headerDTM = headerDTM;
 	}
+	
+// ========================== GETTERS/SETTERS ======================
+	
+	/**
+	 * Gets the DTM table
+	 * 
+	 * @return table of elevations
+	 */
+	public double[][] getTabDTM() {
+		return tabDTM;
+	}
+	
+	/**
+	 * Gets the DTM header
+	 * 
+	 * @return header of the DTM as a KEY/VALUE map
+	 */
+	public Map<String,Double> getHeaderDTM() {
+		return headerDTM;
+	}
+
 
 // ========================== METHODS ==============================
 
