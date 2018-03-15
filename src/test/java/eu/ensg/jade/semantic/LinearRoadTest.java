ec943667be1a1835ac1bc03340ea5ed3a06488a0
AnNoRoca <antoine.roca@ensg.eu>
2018-03-15 15:26:49 +0100

    beginning of tests on linearRoad class

---
+++ src/test/java/eu/ensg/jade/semantic/LinearRoadTest.java
@@ -0,0 +1,26 @@
+package eu.ensg.jade.semantic;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.junit.Test;
+
+import eu.ensg.jade.input.BuildingSHP;
+import eu.ensg.jade.input.OutputRGE;
+
+public class LinearRoadTest {
+
+	/**
+	 * Test method for {@link eu.ensg.jade.semantic.LinearRoad#enlarge())}.
+	 * @throws IOException 
+	 */
+	@Test
+	public void testEnlarge() {
+		
+		//Comment créer une geom? voir la mocker?
+
+	}
+
+}
