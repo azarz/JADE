8a87eda47ba56ee2f59151fce7670cb0f190a1f5
Ogus <ogustin.gagnon@gmail.com>
2018-03-27 15:55:41 +0200

    Changes to centroid type

--- src/main/java/eu/ensg/jade/scene/Scene.java
+++ src/main/java/eu/ensg/jade/scene/Scene.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.vividsolutions.jts.geom.Coordinate;
+
 import eu.ensg.jade.geometricObject.Road;
 import eu.ensg.jade.semantic.Building;
 import eu.ensg.jade.semantic.DTM;
@@ -18,72 +20,67 @@ import eu.ensg.jade.semantic.SurfaceVegetation;
 
 /**
  * Scene is the class implementing the scene and its elements to be created in OpenDS
- * 
+ *
  * @author JADE
  */
 
 public class Scene {
-	
-	
+
+
 // ========================== ATTRIBUTES ===========================
 
 	/**
 	 * List of surfacic roads to create {@link SurfaceRoad}
 	 */
 	private Map <String,Road> roads;
-	
+
 	/**
 	 * List of linear roads to create {@link LineRoad}
 	 */
 	private List<LineRoad> lineRoads;
-	
+
 	/**
 	 * List of buildings to create {@link Building}
 	 */
 	private List<Building> buildings;
-	
+
 	/**
 	 * List of water surfaces to create {@link Hydrography}
 	 */
 	private List<Hydrography> hydrography;
-	
+
 	/**
 	 * List of trees to create {@link Vegetation}
 	 */
 	private List<PointVegetation> vegetation;
-	
+
 	/**
 	 * List of trees area used for tree creation {@link Vegetation}
 	 */
 	private List<SurfaceVegetation> surfaceVegetation;
-	
+
 	/**
 	 * List of street furniture to create {@link StreetFurniture}
 	 */
 	private List<StreetFurniture> streetFurniture;
-	
+
 	/**
 	 * The DTM associated to the scene {@link DTM}
 	 */
-	private DTM dtm;
+	private DTM dtm;	
 	
 	/**
-	 * The X coordinate of the area centroid
+	 * The coordinate of the buildings' centroid
 	 */
-	private double xCentroid = 0;
-	
-	/**
-	 * The Y coordinate of the area centroid
-	 */
-	private double yCentroid = 0;
-	
+	Coordinate buildingCentroid;
+
 	/**
 	 * The collection of intersections between roads
 	 */
 	private IntersectionColl collIntersect;
 
-	
-	
+
+
 // ========================== CONSTRUCTORS =========================
 
 	public Scene() {
@@ -95,31 +92,31 @@ public class Scene {
 		this.surfaceVegetation = new ArrayList<SurfaceVegetation>();
 		this.streetFurniture = new ArrayList<StreetFurniture>();
 	}
-	
+
 
 // ========================== GETTERS/SETTERS ======================
 
 	/**
 	 * Allows to access the list of roads in the scene
-	 * 
+	 *
 	 * @return the scene roads
 	 */
 	public Map<String,Road> getRoads() {
 		return roads;
 	}
-	
+
 	/**
 	 * Allow to set the scene roads
-	 * 
+	 *
 	 * @param roads the list of roads to be assigned
 	 */
 	public void setRoads(Map<String,Road> roads) {
 		this.roads = roads;
 	}
-	
+
 	/**
 	 * Allows to access the list of buildings in the scene
-	 * 
+	 *
 	 * @return the scene buildings
 	 */
 	public List<Building> getBuildings() {
@@ -128,7 +125,7 @@ public class Scene {
 
 	/**
 	 * Allows to set the scene buildings
-	 * 
+	 *
 	 * @param buildings the list of buildings to be assigned
 	 */
 	public void setBuildings(List<Building> buildings) {
@@ -137,7 +134,7 @@ public class Scene {
 
 	/**
 	 * Allows to access the list of water surfaces in the scene
-	 * 
+	 *
 	 * @return the scene water surfaces
 	 */
 	public List<Hydrography> getHydrography() {
@@ -146,7 +143,7 @@ public class Scene {
 
 	/**
 	 * Allows to set the scene water surfaces
-	 * 
+	 *
 	 * @param hydrography the list of water surfaces to be assigned
 	 */
 	public void setHydrography(List<Hydrography> hydrography) {
@@ -155,7 +152,7 @@ public class Scene {
 
 	/**
 	 * Allows to access the list of trees in the scene
-	 * 
+	 *
 	 * @return the scene threes
 	 */
 	public List<PointVegetation> getVegetation() {
@@ -164,7 +161,7 @@ public class Scene {
 
 	/**
 	 * Allows to set the scene vegetation (isolated trees)
-	 * 
+	 *
 	 * @param vegetation the list of trees to be assigned
 	 */
 	public void setVegetation(List<PointVegetation> vegetation) {
@@ -172,7 +169,7 @@ public class Scene {
 	}
 	/**
 	 * Allows to access the list of trees area in the scene
-	 * 
+	 *
 	 * @return the scene trees area
 	 */
 	public List<SurfaceVegetation> getSurfaceVegetation() {
@@ -181,17 +178,17 @@ public class Scene {
 
 	/**
 	 * Allows to set the scene vegetation area
-	 * 
+	 *
 	 * @param vegetation the list of tree surface to be assigned
 	 */
 	public void setSurfaceVegetation(List<SurfaceVegetation> vegetation) {
 		this.surfaceVegetation = vegetation;
 	}
-	
+
 
 	/**
 	 * Allows to access the list of street furniture in the scene
-	 * 
+	 *
 	 * @return the scene street furniture
 	 */
 	public List<StreetFurniture> getStreetFurniture() {
@@ -200,7 +197,7 @@ public class Scene {
 
 	/**
 	 * Allows to set the scene street furniture
-	 * 
+	 *
 	 * @param streetFurniture the list of street furniture to be assigned
 	 */
 	public void setStreetFurniture(List<StreetFurniture> streetFurniture) {
@@ -209,7 +206,7 @@ public class Scene {
 
 	/**
 	 * Allows to access the scene DTM
-	 * 
+	 *
 	 * @return the scene DTM
 	 */
 	public DTM getDtm() {
@@ -218,50 +215,43 @@ public class Scene {
 
 	/**
 	 * Allows to set the scene DTM
-	 * 
+	 *
 	 * @param dtm the DTM to be assigned
 	 */
 	public void setDtm(DTM dtm) {
 		this.dtm = dtm;
 	}
-	
+
 	/**
-	 * Gets the centroid's X
-	 * 
-	 * @return xCentroid
+	 * Gets the building centroid
+	 *
+	 * @return buildingCentroid
 	 */
-	public double getxCentroid() {
-		return xCentroid;
+	public Coordinate getBuildingCentroid() {
+		return buildingCentroid;
 	}
 
 	/**
-	 * Sets the centroid's X
-	 * 
-	 * @param xCentroid
+	 * Sets the building centroid
+	 *
+	 * @param centroid
 	 */
-	public void setxCentroid(double xCentroid) {
-		this.xCentroid = xCentroid;
-	}
-
-	/**
-	 * Gets the centroid's Y
-	 * 
-	 * @return yCentroid
-	 */
-	public double getyCentroid() {
-		return yCentroid;
-	}
-
-	/**
-	 * Sets the centroid's Y
-	 * 
-	 * @param yCentroid
-	 */
-	public void setyCentroid(double yCentroid) {
-		this.yCentroid = yCentroid;
+	public void setBuildingCentroid(Coordinate centroid) {
+		this.buildingCentroid = centroid;
 	}
 	
-	
+	/**
+	 * Sets the building centroid with X and Y
+	 *
+	 * @param x
+	 * @param y
+	 */
+	public void setBuildingCentroid(double x, double y) {
+		this.buildingCentroid.x = x;
+		this.buildingCentroid.y = y;
+	}
+
+
 	/**
 	 * @return the collIntersect
 	 */
@@ -281,45 +271,45 @@ public class Scene {
 
 	/**
 	 * Adds a new road to the existing list of roads
-	 * 
+	 *
 	 * @param newRoad the Road to be added
 	 */
 	public void addRoad(String id, Road newRoad){
 		this.roads.put(id, newRoad);
 	}
-	
+
 	/**
 	 * Adds a new buiding to the existing list of buildings
-	 * 
+	 *
 	 * @param newBuilding the Building to be added
 	 */
 	public void addBuilding(Building newBuilding){
 		this.buildings.add(newBuilding);
 	}
-	
+
 	/**
 	 * Adds a new water surface to the existing list hydrography
-	 * 
+	 *
 	 * @param newWaterSurface the Hydrography to be added
 	 */
 	public void addHydro(Hydrography newWaterSurface){
 		this.hydrography.add(newWaterSurface);
 
 	}
-	
+
 	/**
 	 * Adds a new tree to the existing list of vegetation
-	 * 
+	 *
 	 * @param newTree the three to be added
 	 */
 	public void addVegetation(PointVegetation newTree){
 		this.vegetation.add(newTree);
 
 	}
-	
+
 	/**
 	 * Adds a new street furniture to the existing list of furnitures
-	 * 
+	 *
 	 * @param newFurniture the furniture to be added
 	 */
 	public void addStreetFurniture(StreetFurniture newFurniture){
