249f7096c417bf0de590e4dff246962c89b57a81
AnNoRoca <antoine.roca@ensg.eu>
2018-04-13 16:02:56 +0200

    Added some Javadoc

--- src/main/java/eu/ensg/jade/xml/XMLTerrain.java
+++ src/main/java/eu/ensg/jade/xml/XMLTerrain.java
@@ -13,14 +13,34 @@ import org.w3c.dom.Element;
 public class XMLTerrain implements IXMLExport {
 	
 // ========================== ATTRIBUTES ===========================
-	
+	/**
+	 * The id
+	 */
 	private String id;
+	
+	/**
+	 * The image Height map
+	 */
 	private String imageHeightmap;
 	
+	/**
+	 * The height scale
+	 */
 	private double heightScale;
 	
+	/**
+	 * The patch size
+	 */
 	private int patchSize;
+	
+	/**
+	 * The total size
+	 */
 	private int totalSize;
+	
+	/**
+	 * The distance factor
+	 */
 	private double distanceFactor;
 	
 // ========================== CONSTRUCTORS =========================
@@ -61,51 +81,110 @@ public class XMLTerrain implements IXMLExport {
 	}
 	
 // ========================== GETTERS/SETTERS ======================
-	
+	/**
+	 * Allows to access to the id
+	 * 
+	 * @return The id
+	 */
 	public String getId() {
 		return id;
 	}
 
+	/**
+	 * Allows to set the id
+	 * 
+	 * @param id The new id
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
 
+	/**
+	 * Allows to access to the image height map
+	 * 
+	 * @return The image height map
+	 */
 	public String getImageHeightmap() {
 		return imageHeightmap;
 	}
 
+	/**
+	 * Allows to set the image height map
+	 * 
+	 * @param imageHeightmap The new image height map
+	 */
 	public void setImageHeightmap(String imageHeightmap) {
 		this.imageHeightmap = imageHeightmap;
 	}
 
+	/**
+	 * Allows to access to the height scale
+	 * 
+	 * @return The height scale
+	 */
 	public double getHeightScale() {
 		return heightScale;
 	}
 
+	/**
+	 * Allows to set the height scale
+	 * 
+	 * @param heightScale The new height scale
+	 */
 	public void setHeightScale(double heightScale) {
 		this.heightScale = heightScale;
 	}
 
+	/**
+	 * Allows to access to the patch size
+	 * 
+	 * @return The patch size
+	 */
 	public int getPatchSize() {
 		return patchSize;
 	}
 
+	/**
+	 * Allows to set the patch size
+	 * 
+	 * @param patchSize The new patch size
+	 */
 	public void setPatchSize(int patchSize) {
 		this.patchSize = patchSize;
 	}
 
+	/**
+	 * Allows to access to the total size
+	 * 
+	 * @return The total size
+	 */
 	public int getTotalSize() {
 		return totalSize;
 	}
 
+	/**
+	 * Allows to set the total size
+	 * 
+	 * @param totalSize The new total size
+	 */
 	public void setTotalSize(int totalSize) {
 		this.totalSize = totalSize;
 	}
 
+	/**
+	 * Allows to access to the distance factor
+	 * 
+	 * @return The distance factor
+	 */
 	public double getDistanceFactor() {
 		return distanceFactor;
 	}
 
+	/**
+	 * Allows to set the distance factor
+	 * 
+	 * @param distanceFactor The new distance factor
+	 */
 	public void setDistanceFactor(double distanceFactor) {
 		this.distanceFactor = distanceFactor;
 	}
